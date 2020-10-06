e339ffc9522462c34dd693d1548c54e9578d32b3
Vincent Weltje <vincent.weltje@weareyou.com>
2020-10-06 14:12:02 +0200

    Add 404

--- core/Router.php
+++ core/Router.php
@@ -4,18 +4,21 @@ require_once 'controllers/Dashboard.php';
 require_once 'controllers/Login.php';
 require_once 'controllers/User.php';
 require_once 'controllers/Tickets.php';
+require_once 'controllers/Four0four.php';
 
 class Router {
     public static function getView() {
         $route = self::getRoute();
-        if ($route === "/login") {
+        if ($route === "/") {
+            return new Dashboard();
+        } elseif ($route === "/login") {
             return new Login();
         } elseif ($route === '/logout') {
             return User::logout();
         } elseif ($route === '/new-ticket') {
             return new Tickets();
         }
-        return new Dashboard();
+        return new Four0four();
     }
 
     private static function getRoute() {
