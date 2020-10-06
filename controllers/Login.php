b791e1896d090d12bed3f082b9322fb7002706e6
Vincent Weltje <vincent.weltje@weareyou.com>
2020-10-06 11:30:46 +0200

    Moved is form submit check to formHelper

--- controllers/Login.php
+++ controllers/Login.php
@@ -11,16 +11,12 @@ class Login implements iView {
 
     public function __construct() {
         $this->user = new User();
-        if ($this->isLoginFormSubmit()) {
+        if (FormHelper::isPost()) {
             $this->handleLogin();
         }
         if ($this->user->loggedIn()) {
             header('Location: /');
         }
-    }
-
-    private function isLoginFormSubmit() {
-        return $_SERVER['REQUEST_METHOD'] === 'POST';
     }
 
     private function handleLogin() {
@@ -33,8 +29,6 @@ class Login implements iView {
         } elseif (!$password) {
             $this->errorMessage = 'Please enter your password.';
         } else {
-            $email = $_POST['email'];
-            $password = $_POST['password'];
             $response = $this->user->login($email, $password);
             if (isset($response['error'])) {
                 $this->errorMessage = $response['error'];
--- helpers/FormHelper.php
+++ helpers/FormHelper.php
@@ -1,6 +1,10 @@
 <?php
 
 class FormHelper {
+    public static function isPost() {
+        return $_SERVER['REQUEST_METHOD'] === 'POST';
+    }
+
     public static function getField($fieldName, $type = 'POST') {
         return $_POST[$fieldName] ?? '';
     }
