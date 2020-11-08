f631b359a363394cea0646cade6377e009e61bf0
Vincent Weltje <vincent.weltje@weareyou.com>
2020-11-08 11:54:44 +0100

    Style-up login page

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,19 +1,24 @@
 <div class="container"> 
-    <div id="login" class="flex-column">
-        <h1>Login</h1>
-        <p>Login to continue to the dashboard</p>
-        <?php if (!empty($errorMessage)) : ?>
-            <?= $this->loadView('components/message', [
-                'type' => 'error',
-                'errorMessage' => $errorMessage
-            ]) ?>
-        <?php endif; ?>
-        <form action="/login" method="post">
-            <div class="flex-column">
-                <input type="text" placeholder="Enter your email" name="email" value="<?= FormHelper::getField('email') ?>" />
-                <input type="password" placeholder="Enter your password" name="password" value="<?= FormHelper::getField('password') ?>" />
-            </div>
-            <button type="submit">Login</button>
-        </form>
+    <div id="login">
+        <div id="login-box">
+            <h1>Login</h1>
+            <?php if (!empty($errorMessage)) : ?>
+                <?= $this->loadView('components/message', [
+                    'type' => 'error',
+                    'errorMessage' => $errorMessage
+                ]) ?>
+            <?php endif; ?>
+            <form action="/login" method="post">
+                <div class="form-field">
+                    <label for="email">Email address</label>
+                    <input type="text" placeholder="Enter your email" id="email" name="email" value="<?= FormHelper::getField('email') ?>" />
+                </div>
+                <div class="form-field">
+                    <label for="password">Password</label>
+                    <input type="password" placeholder="Enter your password" id="password" name="password" value="<?= FormHelper::getField('password') ?>" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </div>
 </div>
