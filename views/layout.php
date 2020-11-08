deb3ee384053d32ab3b5d9156d815ac6e494ddf7
Vincent Weltje <vincent.weltje@weareyou.com>
2020-11-08 16:42:46 +0100

    add favicon

--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,7 @@
 <html lang="en">
     <head>
         <title>Ticketify<?= isset($pageTitle) && !empty($pageTitle) ? " | {$pageTitle}" : "" ?></title>
+        <link rel="icon" href="/assets/img/favicon.png" type="image/png">
         <link rel="stylesheet" href="/assets/css/style.css">
         <script type="text/javascript" src="/assets/js/main.js"></script>
     </head>
